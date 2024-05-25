78ac8ae0b2c37a0c42121d4cd37fbf94ce59c4d8
wangbolun <583288277@qq.com>
2024-05-25 22:35:28 +0800

    temp

--- frontend/src/views/ToDoList.vue
+++ frontend/src/views/ToDoList.vue
@@ -8,12 +8,23 @@
         </h1>
         <div class="main">
             <ul class="pl-1">
-                <li class="flex items-center">
+                <li v-for="(item, index) in state.toDoList" :key="index" class="flex items-center">
                     <input type="checkbox" class="inline-flex form-checkbox rounded text-blue-1000 text-2xl" />
-                    <input type="text" class="inline-flex flex-1 ml-4 form-input p-0">
-                    <div class="inline-block w-10">
-                        <button class="inline-block iconfont icon-edit text-xl"></button>
-                        <span class="inline-block iconfont icon-delete text-2xl"></span>
+                    <input v-if="item.isEditing" type="text"
+                        class="inline-flex flex-1 h-10 ml-4 form-input p-0 bg-black leading-7" v-model="item.content">
+                    <span v-else class="inline-flex flex-1 h-10 ml-4 p-0 h-8 leading-10 border-b">
+                        {{ item.content }}
+                    </span>
+                    <div v-if="item.isEditing" class="inline-block w-15">
+                        <span class="inline-block ml-8 iconfont icon-save text-xl text-green-500"
+                            @click="handleConfirmEditToDo(item)"></span>
+                        <span class="inline-block ml-8 iconfont icon-delete text-2xl text-red-500"
+                            @click="handleDeleteToDo"></span>
+                    </div>
+                    <div v-else class="inline-block w-15">
+                        <span class="inline-block ml-8 iconfont icon-edit text-xl" @click="handleEditToDo(item)"></span>
+                        <span class="inline-block ml-8 iconfont icon-delete text-2xl text-red-500"
+                            @click="handleDeleteToDo"></span>
                     </div>
                 </li>
             </ul>
@@ -25,11 +36,31 @@
 <script lang="tsx" setup>
 import { reactive } from 'vue'
 
+interface ToDo {
+    content: string,
+    isEditing: boolean,
+    isChecked: boolean
+}
+
 const state: any = reactive({
     toDoList: [{
-
-    }]
+        content: 'sssssssssssssss',
+        isEditing: false,
+        isChecked: false
+    }] as ToDo[]
 })
+
+const handleEditToDo = (item: ToDo) => {
+    item.isEditing = !item.isEditing
+}
+
+const handleConfirmEditToDo = (item: ToDo) => {
+    item.isEditing = false
+}
+
+const handleDeleteToDo = () => {
+
+}
 </script>
 
 <style>
